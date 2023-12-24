907cad0bbcfc3e1a25b75a305321c89748d76903
Sempiternalll <100777000+Sempiternalll@users.noreply.github.com>
2023-12-24 13:16:07 +0330

    defining toINT function

--- Maze.py
+++ Maze.py
@@ -1,9 +1,12 @@
 import gym
 import gym_maze
 
+def toINT(L):
+    return L[0] * 10 + L[1]
+
 env = gym.make("maze-random-10x10-plus-v0")
 observation = env.reset()
-state = observation
+state = int(toINT(observation))
 Q = np.zeros((100, 4))
 NUM_EPISODES = 1000
 alpha = 0.1
