4a6cf3080fe7c10da4f5f7c43be58d7a494464c3
Sempiternalll <100777000+Sempiternalll@users.noreply.github.com>
2023-12-24 13:30:09 +0330

    Train algorithm applied

--- Maze.py
+++ Maze.py
@@ -13,6 +13,20 @@ alpha = 0.1
 gamma = 0.9
 epsilon = 0.1
 
+for episode in range(100000):
+    if np.random.rand() < epsilon:
+        action = env.action_space.sample()
+    else:
+        action = np.argmax(Q[state, :])
+
+    next_state, reward, done, truncated = env.step(action)
+    next_state = int(toINT(next_state))
+    Q[state, action] += alpha * (reward + gamma * np.max(Q[next_state, :]) - Q[state, action])
+    state = next_state
+
+    if done or truncated:
+        observation = env.reset()
+
 for episode in range(NUM_EPISODES):
     action = env.action_space.sample()
     next_state, reward, done, truncated = env.step(action)
