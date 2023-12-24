fbc410590079ada197e8d12a9587ecea5885c0b7
Sempiternalll <100777000+Sempiternalll@users.noreply.github.com>
2023-12-24 13:03:34 +0330

    Parameters set

--- Maze.py
+++ Maze.py
@@ -3,7 +3,12 @@ import gym_maze
 
 env = gym.make("maze-random-10x10-plus-v0")
 observation = env.reset()
+state = observation
+Q = np.zeros((100, 4))
 NUM_EPISODES = 1000
+alpha = 0.1
+gamma = 0.9
+epsilon = 0.1
 
 for episode in range(NUM_EPISODES):
     action = env.action_space.sample()
